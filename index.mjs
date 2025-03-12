c364b4cb057cf6572c79d22170b784b9bf26a5bb
tw1ttt3r <tw1ttt3r@gmail.com>
2025-03-11 21:40:18 -0600

    feat: implemented barrel and changed msg

--- index.mjs
+++ index.mjs
@@ -1,14 +1,14 @@
 import express from "express";
-import { PORT } from "./config/vars.mjs";
+import { PORT, HOME } from "#config/index.mjs";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express 5 with Bun!");
+app.get(HOME, (req, res) => {
+  res.send("Microservice Roles and Permissions online!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server it's alive!`);
 });
 
 // Exportar la app para que Vercel la reconozca
